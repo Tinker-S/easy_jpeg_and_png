61fee1bb1655c4b8d5f293405d1b35dfe41966f8
Ting Sun <ting.sun@renren-inc.com>
2012-10-23 23:08:55 +0800

    something goes wrong when last commit

--- rrimage.h
+++ rrimage.h
@@ -28,6 +28,32 @@ typedef struct my_error_mgr {
 	jmp_buf setjmp_buffer;
 }* my_error_ptr;
 
-    
+void free_rrimage(rrimage *);
+
+unsigned char get_gray(rrimage *, int, int);
+
+unsigned char get_r(rrimage *, int, int);
+
+unsigned char get_g(rrimage *, int, int);
+
+unsigned char get_b(rrimage *, int, int);
+
+void set_gray(unsigned char gray, rrimage *, int, int);
+
+void set_r(unsigned char r, rrimage *, int, int);
+
+void set_g(unsigned char g, rrimage *, int, int);
+
+void set_b(unsigned char b, rrimage *, int, int);
+
+int check_if_png(FILE *);
+
+rrimage* read_jpeg(FILE *);
+
+int write_jpeg(FILE *, rrimage *);
+
+rrimage* read_png(FILE *);
+
+int write_png(FILE *, rrimage *);
 
 #endif
